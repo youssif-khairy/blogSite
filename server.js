a3e838c9962ac3b40f177af7dc3506336b0982e2
youssif-khairy <youssif.aly@cis.asu.edu.eg>
2020-07-09 23:52:56 +0200

    third commit

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static(__dirname + "/dist"))
 app.listen(process.env.PORT || 4200)
 
 app.get('/*',(req,res)=>{
-    res.sendFile(path.join(__dirname + '/dist/index.html'))
+    res.sendFile(path.join(__dirname + '/dist/blogSite/index.html'))
 })
 
 console.log('listining heerree!!');
